69e433ce6c3bf17306be4a5069edf263d2509e90
bh97 <benjaminhos333@gmail.com>
2019-06-06 11:20:14 +1200

    both requirements for the app now work and only unique prime numbers are returned

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import time
 
 import redis
+from math import sqrt
 from flask import Flask
 
 app = Flask(__name__)
@@ -24,14 +25,20 @@ def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
-@app.route('/isPrime/<number>')
+@app.route('/isPrime/<number>/')
 def isPrime(number):
-    for i in range(2,number):
-        if number % i == 0:
+    print('searching for prime')
+    for i in range(2,int(sqrt(int(number)))):
+        if int(number) % i == 0 or i == int(number):
             return '{} is not a prime number.\n'.format(number)
-    cache.lpush(primes, number)
+    cache.lrem('primes', 0, number)
+    cache.lpush('primes', number)
     return '{} is a prime number.\n'.format(number)
 
-@app.route('/primesStored')
+@app.route('/primesStored/')
 def stored_primes():
-    
+    prime_string = ' \n'
+    primes_list = cache.lrange('primes', 0, -1)
+    for n in primes_list:
+        prime_string = '{} {}\n'.format(prime_string, n)
+    return(prime_string)
